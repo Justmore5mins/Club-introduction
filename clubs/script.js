b27cdc0428d8395bbb0aa481f72f97e6d5b3560d
Justmore5mins <justmore5mins@gmail.com>
2024-09-01 17:29:24 +0800

    add dark mode

--- clubs/script.js
+++ clubs/script.js
@@ -1,16 +1,22 @@
 const club = new URLSearchParams(location.search).get('club')
+
 window.onload = function(){
+    if(JSON.parse(localStorage.getItem("color")) === true){
+        console.log(true)
+        document.body.classList.toggle("dark")
+    }
+
     var img = document.createElement("img")
-    img.src=`http://${location.host}/img/${club}.png`
+    img.src=`http://${location.host}/data/img/${club}.png`
     img.alt="社照"
     img.onload = function(){
         document.getElementById("img").appendChild(img)
     }
     img.onerror = function(){
-        img.src=`http://${location.host}/img/NotFound.png`
+        img.src=`http://${location.host}/data/img/NotFound.png`
         document.getElementById("img").appendChild(img)
     }
-    fetch("data.json")
+    fetch(`http://${location.host}/data/data.json`)
     .then(response => response.json())
     .then(data => {
         document.getElementById("intro").innerHTML = data[club]["介紹"]
@@ -22,7 +28,7 @@ window.onload = function(){
         console.log(members)
         for(const place in members){ // place is int
             let info = members[place] //object
-            table += `<tr><td>|${Object.keys(info)}|</td><td> ${info[Object.keys(info)][0]}</td></tr>`
+            table += `<tr><td>|${Object.keys(info)[0] === "" ? "色即是空" :Object.keys(info)}|</td><td> ${info[Object.keys(info)][0] === "" ? "空即是色" : info[Object.keys(info)][0]}</td></tr>`
         }
         document.getElementById("cadres").innerHTML = table
     })
